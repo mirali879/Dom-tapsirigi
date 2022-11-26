049535ebabb6291d293002a9e5cc32a54294779e
mirali879 <mirali.mammadzade@gmail.com>
2022-11-26 10:37:56 +0200

    firstcommit

---
+++ js.js
@@ -0,0 +1,21 @@
+const msg1= document. getElementById('msg1');
+const msg2= document. getElementById('msg2');
+/* const nmsg= document. getElementById('nmsg'); */
+/* const btn= document.getElementById('btn'); */
+const rsl= document.getElementById('rsl');
+const slc= document.getElementById('slc');
+
+/* function result(){
+    rsl.innerHTML= eval(msg1.value+ slc.value +msg2.value);
+} */
+
+
+/* function result(){
+    nmsg.innerHTML= parseInt(msg1.value)+parseInt(msg2.value);
+}
+ */
+/* 
+btn.addEventListener('click',()=>{
+    nmsg.innerHTML= parseInt(msg1.value)+parseInt(msg2.value);
+
+}) */
